8c3d50241b7d1e2588e8b6406b1b6761922fea2b
David Rodriguez <david@bsdadm.com>
2020-11-06 13:04:33 -0500

    Add functions to get random lowercase, uppercase, numeric and/or a special character

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,26 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//////////////////////////////////////////////////////
+
+// Get a random lowercase letter from the alphabet. 
+function genRandLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// Get a random uppercase letter from the alphabet.
+function genRandUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// Get a random number from 1 to 10.
+function genRandNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+// Get a random symbol from a list of OWASP approved password special characters.
+function genRandSymbol() {
+  const symbols = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
